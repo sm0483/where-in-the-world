d0a0109238aaeced9c561ffc7baa049c2d904cb8
sreerag <msratofficial@gmail.com>
2022-10-04 08:39:01 +0530

    feat:change styling of toggle  button

--- src/comp/Head.js
+++ src/comp/Head.js
@@ -9,7 +9,10 @@ const Head = () => {
         <section className="head-container head-background-color">
             <div className="head-sub-container d-flex justify-content-between align-items-center ">
                 <h1 className='text-color'>Where in the world?</h1>
-                <Switch className='toggle' onChange={changeTheme} checked={theme==="dark"}/>
+                <Switch className='toggle' onChange={changeTheme} checked={theme==="dark"} 
+                onColor={"#202c37"}
+                onHandleColor={'#2b3945'}
+                />
             </div>
         </section>
     );
